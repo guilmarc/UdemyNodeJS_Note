e577bbcf52debae4a36a7ddae0293fc50ff8702f
guilmarc <hk78@live.ca>
2019-10-04 14:19:17 -0400

    Task Manager Session 105

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,32 +1,33 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
-const mongoose = require("mongoose");
-const validator = require("validator");
-
-const User = mongoose.model("User", {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        require: true,
+        required: true,
         trim: true
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
-        validate(value){
-            if(!validator.isEmail(value)) {
-                throw new Error("Email is invalid")
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
             }
         }
     },
     password: {
         type: String,
         required: true,
+        minlength: 7,
         trim: true,
-        minLength: 8,
-        validate(value){
-            if(value.toLowerCase().includes("password")) {
-                throw new Error("Invalid password")
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
             }
         }
     },
@@ -34,11 +35,34 @@ const User = mongoose.model("User", {
         type: Number,
         default: 0,
         validate(value) {
-            if(value < 0) {
-                throw new Error('Age must be a positive number')
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
             }
         }
     }
-});
+})
 
-module.exports = User;
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+    if(!user) throw new Error("Unable to log in")
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch) throw new Error("Unable to login")
+
+    return user
+};
+
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
