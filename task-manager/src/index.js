68a526f2a004ee62929715d4adfbef27d168c722
Marco Guilmette <marco.guilmette@softbiomed.com>
2019-10-08 16:32:35 -0400

    Task Manager (Session 14 Completed)

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 
-
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -9,37 +8,9 @@ require("./db/mongoose");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
-
-
-
-const multer  = require('multer')
-const upload = multer({
-   dest: "images",
-   limits: {
-      fileSize: 1000000       //Limiter à +- 1 megs
-   },
-   fileFilter(req, file, callback) {
-      if(!file.originalname.match(/\.(doc|docx)$/)){
-         return callback(new Error("Please upload a Word document"))
-      }
-
-      callback(undefined, true)
-   }
-});
-
-app.post("/upload", upload.single("upload"), (req, res) => {
-   res.send()
-});
-
-
-
-
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-
-
 
 app.listen(port, ()=> {
    console.log("Server started on port " + port);
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const router = new express.Router();
 const auth = require("../middleware/auth");
+const sharp = require("sharp")
 const multer  = require('multer')
 const upload = multer({
     limits: {
@@ -99,7 +100,9 @@ router.delete('/users/me', auth, async (req, res) => {
 
 router.post("/users/me/avatar", auth, upload.single("avatar"), async (req, res) => {
 
-    req.user.avatar = req.file.buffer
+    //Redimentionner le buffer d'image en Base64 vers PNG en 250x250
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height:250}).png().toBuffer();
+    req.user.avatar = buffer
     await req.user.save();
     res.send();
 }, (error, req, res, next) => {
@@ -118,7 +121,7 @@ router.get("/users/:id/avatar", auth, async (req, res) => {
 
         if(!user || !user.avatar) throw new Error()
 
-        res.set("Content-Type","image/jpg")
+        res.set("Content-Type","image/png")
         res.send(user.avatar)
 
     } catch (e) {
